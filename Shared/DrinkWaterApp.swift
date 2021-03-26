a61018bb374eb97306bb35fd78efea87afa8022f
Mapple <mapplezq@gmail.com>
2021-03-26 10:15:52 +0800

    Initial Commit

---
+++ Shared/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  Shared
+//
+//  Created by Mapple on 2021/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
