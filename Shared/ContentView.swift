3409ab13846badbb7535efd85b44a56587b1afcc
Mapple <mapplezq@gmail.com>
2021-03-26 14:07:57 +0800

    Added drink water function

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isWatered = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                
+                Image(systemName: isWatered ? "crown.fill" : "eyes.inverse")
+                    .font(.system(size: 150))
+                    .foregroundColor(isWatered ? .yellow : .red)
+            }
+            .frame(minHeight: 600)
+            .shadow(radius: 10)
+
+            
+            Divider()
+            
+            Toggle(isOn: $isWatered) {
+                Label("你喝水了吗？", systemImage: "drop.fill")
+            }
+            .foregroundColor(.blue)
+        }
+        .padding()
+
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
